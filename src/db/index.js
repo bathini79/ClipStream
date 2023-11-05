db463068820520898e95504c6cde9a51b46b5bd2
bathini79 <bathininipun79@gmail.com>
2023-11-05 21:23:55 +0530

    feat: added mongo configuration

---
+++ src/db/index.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+const connectDB = async () =>{
+  try {
+    const connectionInstance= await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+    console.log(`MONGODB is CONNECTED , INSTANCE HOST: ${connectionInstance.connection.host}`);
+  } catch (error) {
+    console.log("MONGODB ERROR:",error);
+    process.exit(1)
+  }
+}
+export default connectDB
